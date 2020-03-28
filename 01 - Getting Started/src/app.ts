4d1b6a4e01a024fb4916bd3d01896198644f02a8
Hafez <a.hafez852@gmail.com>
2020-03-28 10:38:59 +0200

    Add decorators examples

--- 01 - Getting Started/src/app.ts
+++ 01 - Getting Started/src/app.ts
@@ -19,14 +19,40 @@ class Person {
 const person = new Person();
 console.log(person);
 
-function Log (target: Product, propertyName: string|Symbol) {
-  console.log('Decorator');
+
+function PropertyLogger (target: any, propertyName: string|Symbol) {
+  console.log('PropertyLogger:');
   console.log(target);
   console.log(propertyName);
+  console.log('\n');
+}
+
+function AccessorLogger (target: any, propertyName: string|Symbol, description: PropertyDescriptor) {
+  console.log('AccessorLogger:');
+  console.log(target);
+  console.log(propertyName);
+  console.log(description);
+  console.log('\n');
+}
+
+function MethodLogger (target: any, name: string|Symbol, description: PropertyDescriptor) {
+  console.log('MethodLogger:');
+  console.log(target);
+  console.log(name);
+  console.log(description);
+  console.log('\n');
+}
+
+function ParameterLogger (target: any, name: string|Symbol, position: number) {
+  console.log('ParameterLogger:');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+  console.log('\n');
 }
 
 class Product {
-  @Log
+  @PropertyLogger
   public title: string;
 
   constructor (title: string, private _price: number) {
@@ -37,13 +63,15 @@ class Product {
     return this._price + 5;
   }
 
+  @AccessorLogger
   set price (price: number) {
     if (price < 0) return;
 
     this._price = price;
   }
 
-  getPriceWithTax (tax: number) {
+  @MethodLogger
+  getPriceWithTax (@ParameterLogger tax: number) {
     return this._price * (1 + tax);
   }
 }
